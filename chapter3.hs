b85592cc0ec11062a27e77765c2217d117f0445e
Daniel Connelly <dconnelly@butterflynetwork.com>
2022-05-04 16:01:10 -0400

    solutions chapter 3

---
+++ chapter3.hs
@@ -0,0 +1,53 @@
+-- exercise 3.1
+
+-- ['a', 'b', 'c'] :: [Char]
+-- ('a', 'b', 'c') :: (Char, Char, Char)
+-- [(False, '0'), (True, '1')] :: [(Bool, Char)]
+-- ([False, True], ['0', '1']) :: ([Bool], [Char])
+-- [tail, init, reverse] :: [[a] -> [a]]
+
+-- exercise 3.2
+
+bools :: [Bool]
+bools = [True, False]
+
+nums :: [[Int]]
+nums = [[i] | i <- [1 .. 10]]
+
+add :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy :: a -> (a, a)
+copy x = (x, x)
+
+apply :: (a -> b) -> a -> b
+apply f x = f x
+
+-- exercise 3.3
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
+
+-- exercise 3.5
+
+-- it isn't possible for all function types to statically determine whether two
+-- functions always produce the same result, because many functions operate on
+-- infinitely many inputs, so it's impossible to verify. further, even for
+-- types with bounded value sets, determining equality requires function
+-- (and therefore program) evaluation and so cannot be done in the type checker
+-- prior to program evaluation
